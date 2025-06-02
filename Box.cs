54f05929b3fbd625e21902186b035d6d5ecf00c4
Najisu <jisusunny0225@gmail.com>
2025-06-02 15:12:48 +0900

    속성(Property)로 구현

--- Box.cs
+++ Box.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,54 +9,52 @@ namespace PropExample
 {
     class Box
     {
+        // C#에서는 속성(Property)
         private int width;
+        
+        public int Width
+        {
+            get { return width;  }
+            set
+            {
+                if(value > 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    Console.WriteLine("너비를 자연수로 초기화해주세요!");
+                }
+            }
+        }
+        
         private int height;
+        public int Heigth
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    Console.WriteLine("높이를 자연수로 초기화해주세요!");
+                }
+            }
+        }
+
+        public int Area
+        {
+            get { return Width * Heigth; }
+        }
 
         public Box(int width, int height)
         {
-            if(width > 0 && height > 0)
-            {
-                this.width = width;
-                this.height = height;
-            }
-            else
-            {
-                Console.WriteLine("너비와 높이는 자연수로 초기화해주세요!");
-            }
+            Width = width;
+            Heigth = height;
         }
 
-        public int getWidth()
-        {
-            return width;
-        }
-        public int getHeight()
-        {
-            return height;
-        }
-
-        public void setWidtht(int width)
-        {
-            if (height > 0)
-            {
-                this.width = width;
-            }
-            else
-            {
-                Console.WriteLine("너비를 자연수로 초기화해주세요!");
-            }
-        }
-        public void setHeight(int height)
-        {
-            if (height > 0)
-            {
-                this.height = height;
-            }
-            else
-            {
-                Console.WriteLine("높이를 자연수로 초기화해주세요!");
-            }
-        }
-
-        public int Area() { return width * height; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -43,6 +43,15 @@ namespace PropExample
             Console.WriteLine(item4.id);
 
             Console.WriteLine(Item.ApplicationName);
+
+            Box box = new Box(100, 100);
+            // Console.WriteLine(box.getHeight());
+            // box.setWidtht(200);
+            // Console.WriteLine(box.Area());
+            Console.WriteLine(box.Heigth);
+            box.Width = 50;
+            Console.WriteLine(box.Area);
+            
         }
 
         // 오버로딩
